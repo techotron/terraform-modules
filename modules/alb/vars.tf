76f325128f37398464a5746305b2331892990c6d
esnow <edward.snow@disneystreaming.com>
2021-09-28 22:23:41 +0100

    add alb module

--- modules/alb/vars.tf
+++ modules/alb/vars.tf
@@ -1,13 +1,19 @@
 variable "environment" {
-  type        = string
-  description = "deployment environment"
+    type        = string
+    description = "deployment environment"
 }
 
 variable "app" {
-  type      = string
+    type        = string
 }
 
 variable "region" {
-  type        = string
-  description = "the aws region"
+    type        = string
+    description = "the aws region"
+}
+
+variable "lb_targetgroup_type" {
+    type        = string
+    description = "target group type [ instance | ip ]"
+    default     = "ip"
 }
