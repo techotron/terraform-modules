570749d0d18ae5d18878090db75cf2f107323a2e
esnow <edward.snow@disneystreaming.com>
2021-09-28 22:16:02 +0100

    add alb module

---
+++ modules/alb/vars.tf
@@ -0,0 +1,13 @@
+variable "environment" {
+  type        = string
+  description = "deployment environment"
+}
+
+variable "app" {
+  type      = string
+}
+
+variable "region" {
+  type        = string
+  description = "the aws region"
+}
